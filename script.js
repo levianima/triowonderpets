42061ff526859067b4ab6375d0b44f57cb7a27b1
levianima <elevianima+github@gmail.com>
2024-12-30 17:18:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Replace these credentials with your desired ones
+    const correctUsername = '09132024';
+    const correctPassword = 'bracelet';
+
+    if (username === correctUsername && password === correctPassword) {
+        window.location.href = './qr.html';
+    } else {
+        alert('Invalid answer. Please try again.');
+    }
+}
